b522aab003e24c2cd1a0706dc2870ff208645dcd
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2025-01-28 15:27:25 +0200

    added Testamonials section

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -3,12 +3,15 @@ import Hero from '../pages/Hero'
 import InfoHome from '../pages/InfoHome'
 import HomeCustomers from '../pages/HomeCustomers'
 import Footer from '../components/Footer'
+import Testimonials from '../components/Testamonials'
+
 const HomeLayout = () => {
   return (
     <>
         <Hero />
         <InfoHome />
         <HomeCustomers />
+        <Testimonials />
         <Footer />
     </>
   )
